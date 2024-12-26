6faa70c51a715b0d975dc8e3b10672a9e7db8dea
Shaddle <diegoomarelizalde@hotmail.com>
2024-12-26 00:11:02 -0600

    Update

---
+++ nvim-data/lazy/pkg-cache.lua
@@ -0,0 +1,10 @@
+return {pkgs={{spec=function()
+return {
+  -- nui.nvim can be lazy loaded
+  { "MunifTanjim/nui.nvim", lazy = true },
+  {
+    "folke/noice.nvim",
+  },
+}
+
+end,dir="C:/Users/Shaddle/AppData/Local/nvim-data/lazy/noice.nvim",name="noice.nvim",file="lazy.lua",source="lazy",},{spec={"nvim-lua/plenary.nvim",lazy=true,},dir="C:/Users/Shaddle/AppData/Local/nvim-data/lazy/plenary.nvim",name="plenary.nvim",file="community",source="lazy",},},version=12,}
